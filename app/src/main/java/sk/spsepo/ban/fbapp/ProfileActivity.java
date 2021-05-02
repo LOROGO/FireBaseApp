ca5717514b0084ec585b18de4b0e477c32f9e429
Rikiboy5 <79976060+Rikiboy5@users.noreply.github.com>
2021-05-02 21:39:15 +0200

    bb

---
+++ app/src/main/java/sk/spsepo/ban/fbapp/ProfileActivity.java
@@ -0,0 +1,13 @@
+package sk.spsepo.ban.fbapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ProfileActivity {
+    private ImageView mProfileImage;
+    private TextView mProfileName, getmProfileStatus, mProfileFriends;
+    private Button mProfileRequest;
+
+}
